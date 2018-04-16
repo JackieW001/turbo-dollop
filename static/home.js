6a3ee25ce8b83e7658f36745365c30c410fed5b2
JackieW001 <jwoo@stuy.edu>
2018-04-16 09:00:55 -0400

    added change color on hover

--- static/home.js
+++ static/home.js
@@ -3,25 +3,27 @@ console.log(data);
 
 var svg = document.getElementById("svg");
 
-var tooltip = d3.select("body")
-    .append("div")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .text("a simple tooltip");
-
 var plot = function(e){
     var cont = d3.select("svg");
 
+    var xscale, yscale, rscale;
     //////// CIRCLES //////////
     var circles = cont.selectAll("circle").data(data).enter();
     circles.append("circle")
-	.attr("cy", function(d) { return (500-(d["McDonald's Per 100,000"] * 50));})
-	.attr("cx", function(d) { return ((d["Income"].replace(",", ""))/50 - 700 );})
-	.attr("r", function(d) { return Math.sqrt(d["Total Population"].replace(",", "").replace(",", "")) / 100;})
+	.attr("cy", function(d) {
+	    yscale = 690-(d["McDonald's Per 100,000"] * 90);
+	    return yscale; })
+	.attr("cx", function(d) {
+	    xscale = (d["Income"].replace(",", ""))/50 - 700;
+	    return xscale; })
+	.attr("r", function(d) {
+	    rscale = Math.sqrt(d["Total Population"].replace(",", "").replace(",", "")) / 100;
+	    return rscale;})
     	.attr("fill", "rgba(0,0,255,0.25)")
 	.attr("stroke", "rgba(0,0,255,1)")
 	.attr("stroke-width", 2)
+	.attr("onmouseover","evt.target.setAttribute('fill','rgba(255,0,0,0.25)'); evt.target.setAttribute('stroke','rgba(255,0,0,1)')")
+	.attr("onmouseout","evt.target.setAttribute('fill','rgba(0,0,255,0.25)'); evt.target.setAttribute('stroke', 'rgba(0,0,255,1)')")
     // tooltip
 	.append("svg:title")
 	.text(function(d) { return d["State"] + "\n" +
@@ -34,30 +36,32 @@ var plot = function(e){
     //////// LINES ///////////
     var lines = cont.selectAll("line").data(data).enter();
     // horizontal line at bottom
-    lines.append("line")
-	.attr("stroke", "lightgrey")
-	.attr("x1", 100)
-	.attr("y1", 450)
-	.attr("x2", 900)
-	.attr("y2", 450)
+    var xaxis = document.createElementNS("http://www.w3.org/2000/svg","line");
+    xaxis.setAttribute("stroke", "lightgrey");
+    xaxis.setAttribute("x1", 100);
+    xaxis.setAttribute("y1", 450);
+    xaxis.setAttribute("x2", 900);
+    xaxis.setAttribute("y2", 450);
+    svg.appendChild(xaxis);
     
     // vertical line on side
-    lines.append("line")
-	.attr("stroke", "lightgrey")
-	.attr("x1", 900)
-	.attr("y1", 100)
-	.attr("x2", 900)
-	.attr("y2", 450)
+    var yaxis = document.createElementNS("http://www.w3.org/2000/svg","line");
+    yaxis.setAttribute("stroke", "lightgrey");
+    yaxis.setAttribute("x1", 900);
+    yaxis.setAttribute("y1", 100);
+    yaxis.setAttribute("x2", 900);
+    yaxis.setAttribute("y2", 450);
+    svg.appendChild(yaxis);
 
     ///////// TEXT ////////////
     var text = cont.selectAll("text").data(data).enter();
 
     text.append("text")
-	.attr("y", function(d) { return 510-(d["McDonald's Per 100,000"] * 50); })
-	.attr("x", function(d) { return ((d["Income"].replace(",", "")/50) - 720 ); })
+	.attr("y", function(d) { return 695 -(d["McDonald's Per 100,000"] * 90); })
+	.attr("x", function(d) { return (d["Income"].replace(",", ""))/50 - 3.5*(d["State"].length) - 700; })
 	.attr("text-shadow", "6px 6px grey")
 	.text(function(d) {
-	    if ((Math.sqrt(d["Total Population"].replace(",", "").replace(",", "")) / 100) > 30){
+	    if ((Math.sqrt(d["Total Population"].replace(",", "").replace(",", "")) / 100) > 25){
 		return d["State"];
 	    }
 	    return "";
@@ -65,18 +69,20 @@ var plot = function(e){
 	})
     
     // X axis text
-    text.append("text")
-	.attr("y", 470)
-	.attr("x", 500)
-	.text("Median Income")
+    var xaxisText = document.createElementNS("http://www.w3.org/2000/svg","text");
+    xaxisText.setAttribute("y", 470);
+    xaxisText.setAttribute("x", 500);
+    xaxisText.innerHTML = "Median Income";
+    svg.appendChild(xaxisText);
 
     // Y axis text
-    text.append("text")
-	.attr("transform", "translate(940,150)rotate(90)")
-	//.attr("y", 10)
-	//.attr("x", 10)
-	.text("Number of McDonalds Per 100,000 People")
-    
+    var yaxisText = document.createElementNS("http://www.w3.org/2000/svg","text");
+    yaxisText.setAttribute("transform", "translate(940,150)rotate(90)");
+    yaxisText.setAttribute("y", 10);
+    yaxisText.setAttribute("x", 10);
+    yaxisText.innerHTML = "Number of McDonalds Per Capita";
+    svg.appendChild(yaxisText);
+
     
     
    
