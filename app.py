401943110746c0cef8baef3022060c2fb849e016
clivejj <cjohnston1@stuy.edu>
2018-04-17 22:19:14 -0400

    added density

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ for line in reader:
 for item in list:
     item.pop("Rank")
 
-print "\nMcDonalds list"
-print list
+#print "\nMcDonalds list"
+#print list
 
 reader = csv.DictReader(open('data/income.csv', 'r'))
 
@@ -32,8 +32,8 @@ for state in income_list:
         if statey['State'] == name:
             statey['Income'] = income
 
-print "\nMedian income list"
-print income_list
+#print "\nMedian income list"
+#print income_list
 
 reader = csv.DictReader(open('data/2014_16_obesity.csv', 'r'))
 obese_list = []
@@ -46,17 +46,26 @@ for item in obese_list:
     item.pop('95% CI')
 
 print "\nObese list"
-print obese_list
+#print obese_list
 
 for lien in list:
     lien["Obesity"] = 0
 
-print list
+#print list
 
 for line in obese_list:
     for row in list:
         if line["State"] == row["State"]:
             row["Obesity"] = line["Prevalence"]
+
+#print list
+
+reader = csv.DictReader(open('data/pop_density.csv', 'r'))
+for item in reader:
+    #print item
+    for row in list:
+        if item['STATE_OR_REGION'] == row["State"]:
+            row["Density"] = item["2010_DENSITY"]
 
 print list
             
@@ -64,6 +73,8 @@ print list
 def root():
     return render_template("home.html", data = list, incomes = income_list, obese = obese_list)
 
+for row in list:
+    print row["Density"]
 
 if __name__ == "__main__":
     my_app.debug = True
